11fb8dd45dd32c454b0ec6320847c8c4642df431
Aitor Rodríguez <aitor.rodriguez990@gmail.com>
2019-10-16 08:32:58 +0200

    Add navbar component

---
+++ components/nav/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './index.scss'
+
+const baseClass = 'da-Nav'
+
+export const Nav = () => {
+  return (
+    <div className={baseClass}>
+      <div />
+      <ul className={`${baseClass}-menu`}>
+        <li className={`${baseClass}-menuItem`}>
+          <a href="#main">Inicio</a>
+        </li>
+        <li className={`${baseClass}-menuItem`}>
+          <a href="#about-me">Sobre mí</a>
+        </li>
+        <li className={`${baseClass}-menuItem`}>
+          <a href="#resources">Recursos</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
