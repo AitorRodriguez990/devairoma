8b505c401334ef7f290826d954e420a53b1db6fb
Aitor Rodríguez <aitor.rodriguez990@gmail.com>
2019-11-20 08:04:36 +0100

    Use data file to generate experience

---
+++ sections/experience/data.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    role: 'Frontend Engineer',
+    company: 'Adevinta',
+    logo: './static/adevinta.jpeg',
+    date: 'Noviembre 2018 - Actualidad',
+    description:
+      'Frontend Engineer del portal inmobiliario Fotocasa. Trabajando en la mejora e iteración del producto por componentes a través de una metodología de trabajo Kanban.',
+    techs: ['ES6', 'React', 'SASS', 'Jenkins', 'Git', 'GutHub']
+  },
+  {
+    role: 'Cofundador y Administrador',
+    company: 'MTBeros',
+    logo: './static/mtberos.jpg',
+    date: 'Mayo 2014 - Actualidad',
+    description:
+      'Cofundador y encargado de la Gestión y Administración de MTBeros, un foro dedicado al ciclismo, especialmente de montaña, donde compartir conocimientos, experiencias y buscar nuevos compañeros de ruta.',
+    techs: []
+  }
+]
--- sections/experience/index.js
+++ sections/experience/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 
 import {Section} from '../../components/section'
 
+import experiences from './data'
 import './index.scss'
 
 const baseClass = 'da-Experience'
@@ -10,51 +11,30 @@ const baseClass = 'da-Experience'
 export const Experience = ({id = ''}) => (
   <Section className={baseClass} title="Experiencia 👨‍💻" id={id}>
     <ul className={`${baseClass}-list`}>
-      <li className={`${baseClass}-listItem`}>
-        <div className={`${baseClass}-listItemImage`}>
-          <img src="./static/adevinta.jpeg" alt="Adevinta" />
-        </div>
-        <div className={`${baseClass}-listItemContent`}>
-          <div className={`${baseClass}-listItemTitle`}>
-            Frontend Engineer
-            <span className={`${baseClass}-listItemCompany`}>Adevinta</span>
-          </div>
-          <div className={`${baseClass}-listItemDate`}>
-            Noviembre 2018 - Actualidad
-          </div>
-          <div className={`${baseClass}-listItemDescription`}>
-            Frontend Engineer del portal inmobiliario Fotocasa. Trabajando en la
-            mejora e iteración del producto por componentes a través de una
-            metodología de trabajo Kanban.
-          </div>
-          <ul className={`${baseClass}-listItemTech`}>
-            <li>React</li>
-            <li>SASS</li>
-            <li>Jenkins</li>
-            <li>Git</li>
-            <li>GitHub</li>
-          </ul>
-        </div>
-      </li>
-      <li className={`${baseClass}-listItem`}>
-        <div className={`${baseClass}-listItemImage`}>
-          <img src="./static/mtberos.jpg" alt="Adevinta" />
-        </div>
-        <div className={`${baseClass}-listItemContent`}>
-          <div className={`${baseClass}-listItemTitle`}>
-            Cofundador y Administrador
-            <span className={`${baseClass}-listItemCompany`}>MTBeros</span>
-          </div>
-          <div className={`${baseClass}-listItemDate`}>
-            Mayo 2014 - Actualidad
-          </div>
-          <div className={`${baseClass}-listItemDescription`}>
-            Cofundador y encargado de la Gestión y Administración de MTBeros, un
-            foro dedicado al ciclismo, especialmente de montaña, donde compartir
-            conocimientos, experiencias y buscar nuevos compañeros de ruta.
-          </div>
-        </div>
-      </li>
+      {experiences.map(({role, company, logo, date, description, techs}) => {
+        const itemClass = `${baseClass}-listItem`
+
+        return (
+          <li className={itemClass}>
+            <div className={`${itemClass}Image`}>
+              <img src={logo} alt={company} />
+            </div>
+            <div className={`${itemClass}Content`}>
+              <div className={`${itemClass}Title`}>
+                {role}
+                <span className={`${itemClass}Company`}>{company}</span>
+              </div>
+              <div className={`${itemClass}Date`}>{date}</div>
+              <div className={`${itemClass}Description`}>{description}</div>
+              <ul className={`${itemClass}Tech`}>
+                {techs.map(tech => (
+                  <li>{tech}</li>
+                ))}
+              </ul>
+            </div>
+          </li>
+        )
+      })}
     </ul>
   </Section>
 )
