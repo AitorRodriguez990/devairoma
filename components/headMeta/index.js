94f46c24f8eab049ef04a1c059e006c64d73ee7b
Aitor Rodríguez <aitor.rodriguez990@gmail.com>
2021-07-14 12:12:22 +0200

    update styles

--- components/headMeta/index.js
+++ components/headMeta/index.js
@@ -1,17 +1,29 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Head from 'next/head'
 
-export const HeadMeta = () => {
+export const HeadMeta = ({section = ''}) => {
+  const name = "Aitor Rodríguez"
+  var title = `${name} | Desarrollo y lanzo proyectos 🚀`
+
+  if (section.length) {
+    title = `${section} | Aitor Rodríguez`
+  }
+
   return (
     <Head>
-      <title>Aitor Rodríguez | Desarrollo y lanzo proyectos 🚀</title>
+      <title>{title}</title>
       <meta
         name="description"
         content="Soy Frontend Engineer en Adevinta Spain y Fundador de MTBeros y otros proyectos (vivos o muertos)."
       />
-      <meta name="author" content="Aitor Rodríguez" />
+      <meta name="author" content={name} />
       <meta httpEquiv="Content-Language" content="es" />
       <link rel="icon" href="/favicon.png" />
     </Head>
   )
+}
+
+HeadMeta.propTypes = {
+  section: PropTypes.string
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import Image from 'next/image'
 import {HeadMeta} from '../components/headMeta'
+import {Header} from '../components/header'
 import {Footer} from '../components/footer'
 
 import styles from './index.module.scss'
 
 const Home = () => (
   <div>
-    <HeadMeta />
+    <HeadMeta /> 
 
-    <div className="page">
+    <div className="page">    
       <div className="container">
         <div className={styles.home}>
           <div className={styles.avatar}>
@@ -22,53 +23,50 @@ const Home = () => (
               width="150"
             />
           </div>
-          <div className={styles.details}>
-            <h1 className={styles.wave}>👋 ¡Hola!</h1>
-            <p className={styles.text}>
-              Mi nombre es Aitor Rodríguez. Soy{' '}
-              <strong>Frontend Engineer</strong> en{' '}
-              <a
-                href="https://adevinta.es/"
-                target="_blank"
-                rel="nofollow noopener noreferrer"
-              >
-                Adevinta Spain
-              </a>{' '}
-              de profesión, y <strong>fundador y administrador</strong> de{' '}
-              <a
-                href="https://www.mtberos.com/"
-                target="_blank"
-                rel="noreferrer nofollow noopener"
-              >
-                MTBeros
-              </a>{' '}
-              como hobbie.
-            </p>
-            <p className={styles.text}>¿Quieres conocerme un poco más?</p>
-            <ul className={styles.list}>
-              <li className={styles.element}>
-                <span className={styles.dot}>👪</span> Soy padre de un pequeño
-                bicho, marido de una compañera maravillosa.
-              </li>
-              <li className={styles.element}>
-                <span className={styles.dot}>🏅</span> Practico ciclismo y
-                running en solitario, y actividades al aire ibre en familia.
-              </li>
-              <li className={styles.element}>
-                <span className={styles.dot}>📚</span> Leo a menudo, aunque
-                también veo series en pareja y películas en muy raras ocasiones.
-              </li>
-              <li className={styles.element}>
-                <span className={styles.dot}>🌍</span> Me gusta viajar, aunque
-                no lo hago todo lo que querría.
-              </li>
-              <li className={styles.element}>
-                <span className={styles.dot}>🤪</span> Soy muy activo y mi
-                cabeza está siempre dando vueltas a ver qué nueva idea se le
-                ocurre.
-              </li>
-            </ul>
-          </div>
+          <h1 className="title">👋 ¡Hola, mi nombre es Aitor Rodríguez!</h1>
+          <p className={styles.text}>
+            Soy{' '}
+            <strong>Frontend Engineer</strong> en{' '}
+            <a
+              href="https://adevinta.es/"
+              target="_blank"
+              rel="nofollow noopener noreferrer"
+            >
+              Adevinta Spain
+            </a>{' '}
+            de profesión, y <strong>fundador y administrador</strong> de{' '}
+            <a
+              href="https://www.mtberos.com/"
+              target="_blank"
+              rel="noreferrer nofollow noopener"
+            >
+              MTBeros
+            </a>{' '}
+            como hobbie.
+          </p>
+          <ul className={styles.list}>
+            <li className={styles.element}>
+              <span className={styles.dot}>👪</span> Padre de un pequeño
+              bicho, marido de una compañera maravillosa.
+            </li>
+            <li className={styles.element}>
+              <span className={styles.dot}>🏅</span> Practico ciclismo y
+              running en solitario, y actividades al aire ibre en familia.
+            </li>
+            <li className={styles.element}>
+              <span className={styles.dot}>📚</span> Leo a menudo, aunque
+              también veo series en pareja y películas en muy raras ocasiones.
+            </li>
+            <li className={styles.element}>
+              <span className={styles.dot}>🌍</span> Me gusta viajar, aunque
+              no lo hago todo lo que querría.
+            </li>
+            <li className={styles.element}>
+              <span className={styles.dot}>🤪</span> Soy muy activo y mi
+              cabeza está siempre dando vueltas a ver qué nueva idea se le
+              ocurre.
+            </li>
+          </ul>
         </div>
       </div>
 
